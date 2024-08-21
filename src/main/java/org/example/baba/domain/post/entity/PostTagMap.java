995536f25193377660623a897c010644b05fc165
Sona <objet_an@naver.com>
2024-08-21 19:18:01 +0900

    [FIX] PostMapTag 엔티티 수정

--- src/main/java/org/example/baba/domain/post/entity/PostTagMap.java
+++ src/main/java/org/example/baba/domain/post/entity/PostTagMap.java
@@ -2,9 +2,10 @@ package org.example.baba.domain.post.entity;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-
+@Getter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PostTagMap {
@@ -12,6 +13,8 @@ public class PostTagMap {
     @Id
     @GeneratedValue
     private Long id;
+
+    private Long name;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "post_id")
